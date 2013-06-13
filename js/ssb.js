030ee30477a33d20dd7e672bb05b0de37f48d004
Eric Buckthal <ebuckthal@gmail.com>
2013-06-13 12:00:19 -0700

    getting close

--- js/ssb.js
+++ js/ssb.js
@@ -1,5 +1,6 @@
 var players = [];
 var platforms = [];
+var score = [0,0];
 
 var cubeVertexPositionBuffer;
 var cubeVertexNormalBuffer;
@@ -12,19 +13,37 @@ var pMatrix = mat4.create();
 var mvStack = [];
 
 var pitch = 0;
-var pitchRate = 0;
-
 var yaw = 0;
-var yawRate = 0;
-
-var speed = 0;
-var xspeed = 0;
-var yspeed = 0;
 
 var currentlyPressedKeys = {};
 
+var ctx;
+var c;
+
+function drawScore() {
+   c = document.getElementById("text");
+   ctx = c.getContext("2d");
+   ctx.font="50px 'Geo'";
+   
+   ctx.clearRect(0,0, c.width, c.height);
+   
+   ctx.fillStyle = "#FF0000";
+   ctx.fillText("damage: " + players[0].knockback, 90, 580);
+   ctx.fillText("score: " + score[1], 90, 600);
+   
+   ctx.fillStyle = "#FF0000";
+   ctx.fillText("damage: " + players[1].knockback, 90, 90);
+   ctx.fillText("score: " + score[0], 90, 110);
+}
+
+
+function drawReset() {
+
+}
+
 function drawCube() {
 
+   setMatrixUniforms();
    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
@@ -35,7 +54,6 @@ function drawCube() {
    //gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, cubeVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-   setMatrixUniforms();
    gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
@@ -126,6 +144,7 @@ function mvPopMatrix() {
 }
 
 function setMatrixUniforms() {
+   
    gl.uniformMatrix4fv( shaderProgram.pMatrixUniform, false, pMatrix );
    gl.uniformMatrix4fv( shaderProgram.mvMatrixUniform, false, mvMatrix );
 
@@ -133,6 +152,9 @@ function setMatrixUniforms() {
    mat4.toInverseMat3(mvMatrix, nMatrix);
    mat3.transpose(nMatrix);
    gl.uniformMatrix3fv( shaderProgram.nMatrixUniform, false, nMatrix );
+   
+   var lightingPosition = [-3, 5 , 10];
+   gl.uniform3fv( shaderProgram.lightPositionUniform, lightingPosition);
 
 }
 
@@ -150,10 +172,10 @@ function Platform(left, right, y, height, depth) {
    function draw() {
       var x = (this.right + this.left) / 2;
       var width = (this.right - this.left);
-      
 
       mvPushMatrix();
-      gl.uniform3f( shaderProgram.diffuseUniform, 0.5, 0.5, 0.5);
+      gl.uniform3f( shaderProgram.diffuseUniform, 203/256, 75/256, 22/256);
+      gl.uniform3f( shaderProgram.ambientUniform, 0.1, 0.1, 0.1);
       mat4.translate(mvMatrix, [x, y - (this.height/2), 0]);
       mat4.scale(mvMatrix, [width, this.height, this.depth]); 
       drawCube();
@@ -176,16 +198,11 @@ function direction(a1, a2) {
    return ([(a1[0]-a2[0]) / d, (a1[1]-a2[1]) / d]);
 }
 
-function testPlayer(p) {
-   p.velocity = [-2,0.5];
-   p.position = [0, 20];
-}
-
 state_enum = {
-STANDING : { code : "S", dcolor : [1.0, 1.0, 0.0] },
-           INAIR : { code : "A", dcolor: [0.0, 0.0, 1.0] },
-           KNOCKBACK : { code : "K", dcolor : [1.0, 0.0, 0.0] },
-           RECOVERY: { code : "R", dcolor : [0.0, 1.0, 0.0] }
+   STANDING : { code : "S", dcolor : [1.0, 1.0, 0.0] },
+   INAIR : { code : "A", dcolor: [0.0, 0.0, 1.0] },
+   KNOCKBACK : { code : "K", dcolor : [1.0, 0.0, 0.0] },
+   RECOVERY: { code : "R", dcolor : [0.0, 1.0, 0.0] }
 }
 
 dir_enum = { NONE : -1, UP : 0, RIGHT : 1, DOWN : 2, LEFT: 3 }
@@ -194,7 +211,7 @@ dir_enum = { NONE : -1, UP : 0, RIGHT : 1, DOWN : 2, LEFT: 3 }
 function Player(id, color, x, y) {
    this.id = id;
    this.state = state_enum.INAIR;
-   this.color = color;
+   this.dcolor = color;
    this.velocity = [0, 0];
    this.position = [x, y];
 
@@ -211,22 +228,32 @@ function Player(id, color, x, y) {
 
    this.dirAttack = dir_enum.NONE; 
 
+   this.knockback = 1;
+   this.deathPos = [0,0];
+
    function draw() {
       mvPushMatrix();
       mat4.translate(mvMatrix, [this.position[0], this.position[1] + 1, 0]);
       mat4.scale(mvMatrix, [2, 2, 2]);
-      
-      //player color
-      gl.uniform3f( shaderProgram.ambientUniform, 0.1, 0.1, 0.1);
-      gl.uniform3f( shaderProgram.diffuseUniform, this.state.dcolor[0], this.state.dcolor[1], this.state.dcolor[2]);
-      drawCube();
+
+      gl.uniform3f( shaderProgram.ambientUniform, 0.05, 0.05, 0.05);
+      if(this.state == state_enum.KNOCKBACK) {
+         gl.uniform3f( shaderProgram.diffuseUniform, 220/256, 50/256, 47/256);
+      } else {
+         gl.uniform3f( shaderProgram.diffuseUniform, this.dcolor[0], this.dcolor[1], this.dcolor[2]);
+      }
 
       if(this.adtimer > 0) { //if we should be drawing
-         mvPushMatrix();
+         var r = (this.adtimer / 200) * 2 * Math.PI - Math.PI;
+         var s = Math.min((this.adtimer / 200) * 2, 1 - (this.adtimer / 200) * 2);
          switch(this.dirAttack) {
             case dir_enum.UP:
+               mat4.rotate(mvMatrix, r, [1, 0, 0]);
+               mvPushMatrix();
                mat4.translate(mvMatrix, [0, 1, 0]);
-               mat4.scale(mvMatrix, [2, 2, 2]);
+               mat4.scale(mvMatrix, [1.5, 2, 1.5]);
+               drawCube();
+               mvPopMatrix();
                break;
             case dir_enum.LEFT:
                mat4.translate(mvMatrix, [-1, 0, 0]);
@@ -243,19 +270,26 @@ function Player(id, color, x, y) {
             default:
                break;
          }
-         drawCube();
-         mvPopMatrix();
+      } else if (this.deathtimer > 0) {
+         this.drawPlayerDeath(this.deathPos);
+      } else {
       }
 
+      drawCube();
       mvPopMatrix();
    }
    this.draw = draw;
+
+   function drawPlayerDeath(pos, deathtimer) {
+
+   }
+   this.drawPlayerDeath = drawPlayerDeath;
 
    function attack(direction) {
       if(this.artimer > 0) {
          return
       }
-      this.adtimer = 400;
+      this.adtimer = 200;
       this.artimer = 800;
       switch(this.state) {
          case state_enum.STANDING:
@@ -390,16 +424,19 @@ function Player(id, color, x, y) {
 
       switch(this.state) {
          case state_enum.STANDING:
-            
+
             var j = this.checkForHit(); //j[0] = player number, j[1] = direction of knockback
 
             if(j[0] > -1) {
                console.log("hit by: " + j[0]);
                console.log(j[1]);
                this.state = state_enum.KNOCKBACK;
-               this.velocity[0] = (j[1][0]);
-               this.velocity[1] = (j[1][1]);
-               this.rtimer = 400;
+               this.velocity[0] = (j[1][0]) * 1.2 * this.knockback;
+               this.velocity[1] = (j[1][1]) * 1.2 * this.knockback;
+               this.rtimer = 400 * 1.2 * this.knockback;
+               this.knockback += 1;
+               
+               drawScore();
                break;
             }
 
@@ -412,13 +449,18 @@ function Player(id, color, x, y) {
             //gravity
             this.velocity[1] += (-0.001*elapsed);
             this.velocity[0] /= 1.01;
-            
+
             var k = this.checkForDeath();
             if(k[0] > -1) {
                console.log("death");
+               this.knockback = 1;
                this.state = state_enum.INAIR;
                this.position = [0, 30];
                this.velocity = [0, 0];
+
+               score[this.id] += 1;
+               this.deathtimer = 1000;
+               drawScore();
                break;
             }
 
@@ -427,9 +469,12 @@ function Player(id, color, x, y) {
                console.log("hit by: " + j[0]);
                console.log(j[1]);
                this.state = state_enum.KNOCKBACK;
-               this.velocity[0] = (j[1][0]);
-               this.velocity[1] = (j[1][1]);
-               this.rtimer = 400;
+               this.velocity[0] = (j[1][0]) * 1.2 * this.knockback;
+               this.velocity[1] = (j[1][1]) * 1.2 * this.knockback;
+               this.rtimer = 400 * 1.2 * this.knockback;
+               this.knockback += 1;
+               
+               drawScore();
                break;
             }
 
@@ -451,20 +496,25 @@ function Player(id, color, x, y) {
 
             //gravity
             this.velocity[1] += (-0.001*elapsed);
-            
+
             if(this.rtimer > 0) {
                this.rtimer -= elapsed;
             } else {
                this.state = state_enum.INAIR;
                break;
             }
-            
+
             var k = this.checkForDeath();
             if(k[0] > -1) {
                console.log("death");
+               this.knockback = 1;
                this.state = state_enum.INAIR;
                this.position = [0, 30];
                this.velocity = [0, 0];
+               
+               this.deathtimer = 1000;
+               score[this.id] += 1;
+               drawScore();
                break;
             }
 
@@ -506,7 +556,7 @@ function Player(id, color, x, y) {
    this.tick = tick;
 
    function checkForDeath() {
-      if(this.position[1] < -30 || this.position[1] > 50 || this.position[0] < -50 || this.position[0] > 100) {
+      if(this.position[1] < -50 || this.position[1] > 150 || this.position[0] < -150 || this.position[0] > 150) {
          return [1, 0];
       }
       return [-1, 0];
@@ -547,7 +597,7 @@ function Player(id, color, x, y) {
          var tempx = players[i].position[0] + offset[0];
          var tempy = players[i].position[1] + offset[1];
 
-         if(distance(this.position, [tempx, tempy]) < 3) {
+         if(distance(this.position, [tempx, tempy]) < 2) {
             return [i, direction(this.position, [tempx, tempy])];
          }
 
@@ -563,17 +613,21 @@ function Player(id, color, x, y) {
                this.position[0] <= platforms[i].right &&
                this.position[1] >= platforms[i].y &&
                this.position[1] + this.velocity[1] + this.vmod[1] <= platforms[i].y) {
-            return i; //return platform number
-         }
+                  return i; //return platform number
+               }
       } 
       return -1;
    }
    this.checkForPlatforms = checkForPlatforms;
 }
 
+function initScore() {
+   drawScore();
+}
+
 function initPlayers() {
-   players.push(new Player(0, [1.0, 0.0], -5, 30));
-   players.push(new Player(1, [0.0, 1.0], 5, 30));
+   players.push(new Player(0, [181/256, 137/256, 0/256], -5, 30));
+   players.push(new Player(1, [108/256, 113/256, 196/256], 5, 30));
 }
 
 function initPlatforms() {
@@ -598,25 +652,23 @@ function setupCamera() {
    zPos = Math.max(max[0]-min[0], max[1]-min[1]) * 1.5 + 20;
 
    zPos = Math.max(30, zPos);
+   
 
    mat4.perspective(60, gl.viewportWidth / gl.viewportHeight, 0.1, 1000.0, pMatrix);
    mat4.identity(mvMatrix);
+   
+   
+   
    mat4.rotate(mvMatrix, degToRad(-pitch), [1,0,0]);
    mat4.rotate(mvMatrix, degToRad(-yaw), [0,1,0]);
    mat4.translate(mvMatrix, [-xPos, -yPos, -zPos]);
-
-   //console.log("xPos: " + xPos + " yPos: " + yPos + " zPos: " + zPos);
-
+   
+   
 }
 
 function drawScene() {
-   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
+   
    setupCamera();
-
-   var lightingPosition = [4, 4, 4];
-   gl.uniform3fv( shaderProgram.lightPositionUniform, lightingPosition);
 
    for(var i = 0, len=players.length; i < len; i++) {
       players[i].draw();
@@ -635,11 +687,6 @@ function animate() {
    if (lastTime != 0) {
       var elapsed = timeNow - lastTime;
 
-      zPos -= Math.cos(degToRad(yaw)) * speed * elapsed;
-      //xPos -= Math.sin(degToRad(yaw)) * speed * elapsed;
-      yPos -= yspeed * elapsed;
-      xPos -= xspeed * elapsed;
-
       for(var i = 0; i < players.length; i++) {
          players[i].tick(elapsed);
       }
@@ -647,22 +694,14 @@ function animate() {
       for(var i = 0; i < platforms.length; i++) {
          platforms[i].animate(elapsed);
       }
-
    }
    lastTime = timeNow;
-}
-
-function resolveCollisions() {
-
-   for(var py = 0; py < players.length; py++) {
-
-   }
 }
 
 function tick() {
    requestAnimFrame(tick);
    handleKeys();
    animate();
-   resolveCollisions();
    drawScene();
+   
 }
